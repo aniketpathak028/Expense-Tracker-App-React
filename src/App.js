2056866d714312d72337ffe28ec87cb6b5e36463
Aniket Pathak <aniketpathak028@gmail.com>
2021-11-05 23:11:44 +0530

    add NewExpense component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -25,6 +27,7 @@ function App() {
 
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
